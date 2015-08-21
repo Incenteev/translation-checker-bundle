52893d0f6dae5dbef0221ed3cc88ce6ccea0e748
Christophe Coevoet <stof@notk.org>
2015-08-21 16:12:04 +0200

    Fix compatibility with Symfony 2.7 and enable translation caching
    The previous implementation was overwriting all options to disable
    caching to avoid reusing the same cache than the normal translator for
    our translator without fallback.
    This does not work anymore in Symfony 2.7 as resources are also injected
    through the options and so we need to preserve them.
    Options are not reset entirely anymore, but the cache folder is
    overwritten instead.
    This also optimizes performance as the translations are cached.

---
+++ DependencyInjection/Compiler/ConfigureTranslatorPass.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Incenteev\TranslationCheckerBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ConfigureTranslatorPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('incenteev_translation_checker.exposing_translator')) {
+            return;
+        }
+
+        if (!$container->has('translator.default')) {
+            return;
+        }
+
+        $translatorDef = $container->findDefinition('translator.default');
+        $options = $translatorDef->getArgument(3);
+
+        if (!is_array($options)) {
+            // Weird setup. Reset all options
+            $options = array();
+        }
+
+        // use a separate cache as we have no fallback locales
+        $options['cache_dir'] = '%kernel.cache_dir%/incenteev_translations';
+
+        $container->findDefinition('incenteev_translation_checker.exposing_translator')
+            ->replaceArgument(3, $options);
+    }
+}
--- IncenteevTranslationCheckerBundle.php
+++ IncenteevTranslationCheckerBundle.php
@@ -2,8 +2,14 @@
 
 namespace Incenteev\TranslationCheckerBundle;
 
+use Incenteev\TranslationCheckerBundle\DependencyInjection\Compiler\ConfigureTranslatorPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class IncenteevTranslationCheckerBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new ConfigureTranslatorPass());
+    }
 }
