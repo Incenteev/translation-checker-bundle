5485e6b49b24b318d8c070f5a93a6d83a555988d
Christophe Coevoet <stof@notk.org>
2017-06-12 18:49:28 +0200

    Add a functional test for the bundle
    This bundle is very sensitive to changes in the way the Symfony translator
    service gets configured, due to the magic it uses to reuse the configuration
    with a few changes to some options.
    The functional test ensures that this logic actually works fine.

---
+++ Tests/FixtureApp/TestKernel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Incenteev\TranslationCheckerBundle\Tests\FixtureApp;
+
+use Incenteev\TranslationCheckerBundle\IncenteevTranslationCheckerBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+
+class TestKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return array(
+            new FrameworkBundle(),
+            new IncenteevTranslationCheckerBundle(),
+        );
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(function (ContainerBuilder $container) {
+            $container->loadFromExtension('framework', array(
+                'translator' => array('fallback' => 'en'),
+                'secret' => 'test',
+            ));
+        });
+    }
+
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir().'/incenteev_translation_checker';
+    }
+
+    public function getLogDir()
+    {
+        return $this->getCacheDir();
+    }
+}
