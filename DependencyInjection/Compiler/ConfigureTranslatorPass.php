74bddf3bd5f6dfd1df4e6c14e1eb3a6e4fdc157b
Christophe Coevoet <stof@notk.org>
2017-06-12 18:51:25 +0200

    fix the configuration of the translator for Symfony 3.3+

--- DependencyInjection/Compiler/ConfigureTranslatorPass.php
+++ DependencyInjection/Compiler/ConfigureTranslatorPass.php
@@ -18,7 +18,15 @@ class ConfigureTranslatorPass implements CompilerPassInterface
         }
 
         $translatorDef = $container->findDefinition('translator.default');
-        $options = $translatorDef->getArgument(3);
+
+        // Symfony 3.3 added the default locale as third argument, before the loader ids and the options
+        if (is_string($translatorDef->getArgument(2))) {
+            $optionsArgumentIndex = 4;
+        } else {
+            $optionsArgumentIndex = 3;
+        }
+
+        $options = $translatorDef->getArgument($optionsArgumentIndex);
 
         if (!is_array($options)) {
             // Weird setup. Reset all options
@@ -29,6 +37,6 @@ class ConfigureTranslatorPass implements CompilerPassInterface
         $options['cache_dir'] = '%kernel.cache_dir%/incenteev_translations';
 
         $container->findDefinition('incenteev_translation_checker.exposing_translator')
-            ->replaceArgument(3, $options);
+            ->replaceArgument($optionsArgumentIndex, $options);
     }
 }
