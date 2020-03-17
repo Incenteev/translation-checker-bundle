19879832e16d48f1b0435fefb95eddee715cb047
Christophe Coevoet <stof@notk.org>
2020-03-17 20:59:07 +0100

    Fix deprecated usage of the config component

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,8 +9,15 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('incenteev_translation_checker')
+        $treeBuilder = new TreeBuilder('incenteev_translation_checker');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('incenteev_translation_checker');
+        }
+
+        $rootNode
             ->children()
                 ->arrayNode('extraction')
                     ->addDefaultsIfNotSet()
